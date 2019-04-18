00f28e336502f2644056c5d6673a48f1eb17a212
JuanFLopez <juanfxlopez@gmail.com>
2019-04-18 09:38:58 +0200

    adding checkpoints and fixing multiGPU use

--- Resnet101Linknet/modules/helper.py
+++ Resnet101Linknet/modules/helper.py
@@ -1,4 +1,6 @@
+import os
 import torch
+from torch import optim
 
 def jaccard(y_true, y_pred):
     intersection = (y_true * y_pred).sum()
@@ -19,3 +21,39 @@ def load_model(cust_model, model_dir="dense_segm.pt", map_location_device="cpu")
     cust_model.eval()
     return cust_model
 
+def save_checkpoint(model, optimizer,epoch,epoch_loss,aver_jaccard,aver_jaccard_inter):
+    checkpoint = { 'Epochs': epoch,
+        'model': model,
+        'state_dict': model.state_dict(),
+        'optimizer' : optimizer.state_dict(),
+        'epoch_loss': epoch_loss,
+        'aver_jaccard': aver_jaccard,
+        'aver_jaccard_inter': aver_jaccard_inter}
+    checkpoint_name= "checkpoint_epoch" + str(epoch) + ".pth"
+    torch.save(checkpoint, checkpoint_name)
+    print(checkpoint_name)
+    if (epoch>1):
+        checkpoint_previousname= "checkpoint_epoch" + str(epoch-1) + ".pth"
+        os.remove(checkpoint_previousname)
+
+def load_checkpoint(model,filepath):
+    use_cuda = torch.cuda.is_available()
+    if use_cuda:
+        checkpoint = torch.load(filepath)
+        model.cuda()
+    else:
+        checkpoint = torch.load(filepath ,map_location="cpu")
+    
+    '''for parameter in model.parameters():
+        #parameter.requires_grad = False
+        print (parameter.requires_grad)'''
+
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer = optim.Adam(model.parameters(), lr = 0.0001)
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    phase='train'
+    print("| {} Loss: {:.4f} | Jaccard Average Acc: {:.4f} | Jaccard Average Acc inter: {:.4f} |".format(phase, checkpoint['epoch_loss'], checkpoint['aver_jaccard'],checkpoint['aver_jaccard_inter']))
+    print("_"*15)
+    #model.eval()
+    #model.train()
+    return model,optimizer
