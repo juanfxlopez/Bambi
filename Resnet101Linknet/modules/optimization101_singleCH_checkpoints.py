ed92f9504897e0afd6d826938036a8687af7af1c
Juan F. Lopez <juanfxlopez@gmail.com>
2019-04-29 18:12:37 +0200

    Update optimization101_singleCH_checkpoints.py

--- Resnet101Linknet/modules/optimization101_singleCH_checkpoints.py
+++ Resnet101Linknet/modules/optimization101_singleCH_checkpoints.py
@@ -22,7 +22,7 @@ args = vars(ap.parse_args())
 
 use_cuda = torch.cuda.is_available()
 # Hyperparameters
-batch_size = 64
+batch_size = 16
 nr_epochs = 20
 momentum = 0.95
 lr_rate = 0.03
@@ -34,7 +34,7 @@ gamma = 0.5
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #use_cuda = False
 
-segm_model = ResNetLinkModel(input_channels=1, pretrained=True, num_classes=1)
+segm_model = ResNetLinkModel(input_channels=1, pretrained=True, num_classes=2)
 
 if torch.cuda.device_count() > 1:
      #dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
@@ -119,29 +119,29 @@ def train_model(cust_model, dataloaders, criterion, optimizer, num_epochs, sched
                 #print(preds)
                 preds=torch.cat(preds)
                 print(preds.shape)
-                jaccard_acc += jaccard(labels, torch.sigmoid(preds))
-                #jaccard_acc_inter += jaccard(inter, torch.sigmoid(preds))
+                jaccard_acc += jaccard(labels, torch.sigmoid(preds)) # THIS IS THE ONE THAT STILL IS ACCUMULATION IN ONLY ONE GPU
+                jaccard_acc_inter += jaccard(inter, torch.sigmoid(preds))
                 #dice_acc += dice(labels, preds)
             
             epoch_loss = running_loss / len(dataloaders[phase])
-            #aver_jaccard = jaccard_acc / len(dataloaders[phase])
-            #aver_jaccard_inter = jaccard_acc_inter / len(dataloaders[phase])
+            aver_jaccard = jaccard_acc / len(dataloaders[phase])
+            aver_jaccard_inter = jaccard_acc_inter / len(dataloaders[phase])
             #aver_dice = dice_acc / len(dataloaders[phase])
 
-            print("| {} Loss: {:.4f} | Jaccard Average Acc: {:.4f} | Jaccard Average Acc inter: {:.4f} |".format(phase, epoch_loss, aver_jaccard))
+            print("| {} Loss: {:.4f} | Jaccard Average Acc: {:.4f} | Jaccard Average Acc inter: {:.4f} |".format(phase, epoch_loss, aver_jaccard, aver_jaccard_inter))
             print("_"*15)
-            '''if phase == "valid" and aver_jaccard > best_acc:
+            if phase == "valid" and aver_jaccard > best_acc:
                 best_acc = aver_jaccard
-                #best_acc_inter = aver_jaccard_inter
+                best_acc_inter = aver_jaccard_inter
                 best_epoch_loss = epoch_loss
                 #best_model_wts = copy.deepcopy(cust_model.state_dict)
                 best_model_wts = copy.deepcopy(cust_model)
                 best_optimizer_wts = optim.Adam(best_model_wts.parameters(), lr = 0.0001)
                 best_optimizer_wts.load_state_dict(optimizer.state_dict())
             if phase == "valid":
-                val_acc_history.append(aver_jaccard)'''
+                val_acc_history.append(aver_jaccard)
         print("^"*15)
-        #save_checkpoint(best_model_wts,best_optimizer_wts,epoch+1,best_epoch_loss,best_acc,best_acc_inter)
+        save_checkpoint(best_model_wts,best_optimizer_wts,epoch+1,best_epoch_loss,best_acc,best_acc_inter)
         print(" ")
         scheduler.step()
     time_elapsed = time.time() - start_time
