83a059d543ec097fbf8f89c160ca083266f8b350
Juan F. Lopez <juanfxlopez@gmail.com>
2019-04-29 17:48:33 +0200

    Update optimization101_singleCH_checkpoints.py

--- Resnet101Linknet/modules/optimization101_singleCH_checkpoints.py
+++ Resnet101Linknet/modules/optimization101_singleCH_checkpoints.py
@@ -117,20 +117,20 @@ def train_model(cust_model, dataloaders, criterion, optimizer, num_epochs, sched
                 print(labels.shape)
                 #preds=torch.FloatTensor(preds)
                 #print(preds)
-                preds=torch.cat(preds)
-                print(preds.shape)
-                jaccard_acc += jaccard(labels, preds)
+                #preds=torch.cat(preds)
+                #print(preds.shape)
+                #jaccard_acc += jaccard(labels, preds)
                 #jaccard_acc_inter += jaccard(inter, torch.sigmoid(preds))
                 #dice_acc += dice(labels, preds)
             
             epoch_loss = running_loss / len(dataloaders[phase])
-            aver_jaccard = jaccard_acc / len(dataloaders[phase])
+            #aver_jaccard = jaccard_acc / len(dataloaders[phase])
             #aver_jaccard_inter = jaccard_acc_inter / len(dataloaders[phase])
             #aver_dice = dice_acc / len(dataloaders[phase])
 
             print("| {} Loss: {:.4f} | Jaccard Average Acc: {:.4f} | Jaccard Average Acc inter: {:.4f} |".format(phase, epoch_loss, aver_jaccard))
             print("_"*15)
-            if phase == "valid" and aver_jaccard > best_acc:
+            '''if phase == "valid" and aver_jaccard > best_acc:
                 best_acc = aver_jaccard
                 #best_acc_inter = aver_jaccard_inter
                 best_epoch_loss = epoch_loss
@@ -139,7 +139,7 @@ def train_model(cust_model, dataloaders, criterion, optimizer, num_epochs, sched
                 best_optimizer_wts = optim.Adam(best_model_wts.parameters(), lr = 0.0001)
                 best_optimizer_wts.load_state_dict(optimizer.state_dict())
             if phase == "valid":
-                val_acc_history.append(aver_jaccard)
+                val_acc_history.append(aver_jaccard)'''
         print("^"*15)
         #save_checkpoint(best_model_wts,best_optimizer_wts,epoch+1,best_epoch_loss,best_acc,best_acc_inter)
         print(" ")
