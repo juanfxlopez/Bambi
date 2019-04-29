af767f61b88fd0ffc60d5542fca31f503b86e616
Juan F. Lopez <juanfxlopez@gmail.com>
2019-04-29 14:06:33 +0200

    Update optimization101_singleCH_checkpoints.py

--- Resnet101Linknet/modules/optimization101_singleCH_checkpoints.py
+++ Resnet101Linknet/modules/optimization101_singleCH_checkpoints.py
@@ -54,7 +54,7 @@ mul_transf = [ transforms.Resize(size=(img_size, img_size)), transforms.ToTensor
 optimizer= optim.Adam(segm_model.parameters(), lr = 0.0001)
 #criterion = nn.BCEWithLogitsLoss().cuda() if use_cuda else nn.BCEWithLogitsLoss()
 criterion = nn.BCEWithLogitsLoss()
-criterion = DataParallelCriterion(criterion)
+#criterion = DataParallelCriterion(criterion)
 criterion.to(device)
 
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=gamma)
