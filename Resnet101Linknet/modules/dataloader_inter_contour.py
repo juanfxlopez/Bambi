4f4a08e1861058f876eb20924dcecdd4734406ed
JuanFLopez <juanfxlopez@gmail.com>
2019-05-02 10:44:58 +0200

    adding trainining code for 3 channels network

---
+++ Resnet101Linknet/modules/dataloader_inter_contour.py
@@ -0,0 +1,122 @@
+from torch.utils.data import Dataset, DataLoader
+import os
+import pandas as pd
+from PIL import Image
+import numpy as np
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from skimage import io
+
+class DatasetCells(Dataset):
+    """An abstract class representing a Dataset.
+    All other datasets should subclass it. All subclasses should override
+    ``__len__``, that provides the size of the dataset, and ``__getitem__``,
+    supporting integer indexing in range from 0 to len(self) exclusive.
+    """
+    def __init__(self,file_data_idx, file_label_idx, file_inter_idx, file_contour_idx, transform=None, mode="train"):
+        self.data_root = "../data/GenData/"
+        #self.data = pd.read_csv(file_path)
+        self.file_data_idx = file_data_idx
+        self.file_label_idx = file_label_idx
+        self.file_inter_idx = file_inter_idx
+        self.file_inter_idx = file_contour_idx
+        self.transform = transform
+        self.mode = mode
+        if self.mode is "train":
+          self.data_dir = os.path.join(self.data_root, "TrainData/images/")
+          self.label_dir = os.path.join(self.data_root, "TrainData/labels/")
+          self.inter_dir = os.path.join(self.data_root, "TrainData/watershed/")
+          self.contour_dir = os.path.join(self.data_root, "TrainData/contours/")
+        elif self.mode is "validation":
+            pass
+        elif self.mode is "test":
+            pass
+
+    def __len__(self):
+        return len(self.file_data_idx)
+    
+    def __getitem__(self, index):
+        # load image as ndarray type (Height * Width * Channels)
+        # be carefull for converting dtype to np.uint8 [Unsigned integer (0 to 255)]
+        # in this example, i don't use ToTensor() method of torchvision.transforms
+        # so you can convert numpy ndarray shape to tensor in PyTorch (H, W, C) --> (C, H, W)
+        #image = self.data.iloc[index, 1:].values.astype(np.uint8).reshape((1, 28, 28))
+        #label = self.data.iloc[index, 0]
+        #train_id = self.file_data_idx["ids"].iloc[index]
+        #label_id = self.file_data_idx["ids"].iloc[index]
+        if self.mode is "train":
+          file_id = self.file_data_idx["ids"].iloc[index]
+          self.image_path = os.path.join(self.data_dir, file_id)
+          self.label_path = os.path.join(self.label_dir, file_id)# file id should be from label data, buit in this case is the same.
+          self.inter_path = os.path.join(self.inter_dir, file_id)
+          self.contour_path = os.path.join(self.contour_dir, file_id)
+          #image = io.imread(self.image_path)
+          #label = io.imread(self.label_path)
+          #inter = io.imread(self.inter_path)
+          image = Image.open(self.image_path).convert("L")# L -> grayscale channel
+          label = Image.open(self.label_path)
+          inter = Image.open(self.inter_path)
+          contour = Image.open(self.contour_path)
+
+          
+          if self.transform is not None:
+              image = self.transform(image)
+              label = self.transform(label)
+              inter = self.transform(inter)   
+              contour = self.transform(contour)      
+          return image, label, inter,contour
+        if self.mode is "validation":
+            pass
+        if self.mode is "test":
+            pass
+
+    def __add__(self, other):
+        return ConcatDataset([self, other])
+
+
+def CellTrainData(data_transform=None, mode="train"):
+    file_idxs = pd.read_csv("../data/GenData/train_input_ids.csv")
+    label_idxs = pd.read_csv("../data/GenData/train_labels_ids.csv")
+    inter_idxs = pd.read_csv("../data/GenData/train_inter_ids.csv")
+    contour_idxs = pd.read_csv("../data/GenData/train_contour_ids.csv")
+    dataset = DatasetCells(file_idxs, label_idxs, inter_idx, transform=data_transform, mode=mode)
+    return dataset
+
+def CellDataLoader(data_transform=None, mode="train", batch_sz=2, workers=1):
+    file_idxs = pd.read_csv("../data/GenData/train_input_ids.csv")
+    label_idxs = pd.read_csv("../data/GenData/train_labels_ids.csv")
+    inter_idxs = pd.read_csv("../data/GenData/train_inter_ids.csv")
+    contour_idxs = pd.read_csv("../data/GenData/train_contour_ids.csv")
+    if data_transform is None:
+        data_transform = transforms.ToTensor()
+    dataset = DatasetCells(file_idxs, label_idxs, inter_idxs, transform=data_transform, mode="train")
+    dataloader = DataLoader(dataset, batch_size=batch_sz, num_workers=workers, shuffle=True)
+    return dataloader
+
+def CellTrainValidLoader(data_transform=None, validation_split=0.1, mode="train", batch_sz=2, workers=1):
+    file_idxs = pd.read_csv("../data/GenData/train_input_ids.csv")
+    label_idxs = pd.read_csv("../data/GenData/train_labels_ids.csv")
+    inter_idxs = pd.read_csv("../data/GenData/train_inter_ids.csv")
+    contour_idxs = pd.read_csv("../data/GenData/train_contour_ids.csv")
+    if data_transform is None:
+        data_transform = transforms.ToTensor()
+    dataset = DatasetCells(file_idxs, label_idxs,inter_idxs,contour_idxs, transform=data_transform, mode="train")
+    
+    shuffle_dataset = True
+    random_seed = 1234
+
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+    train_indices, val_indices = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = DataLoader(dataset, batch_size=batch_sz, sampler = train_sampler, num_workers=workers)
+    validation_loader = DataLoader(dataset, batch_size=batch_sz, sampler = valid_sampler, num_workers=workers)
+    
+    return train_loader, validation_loader
---
+++ Resnet101Linknet/modules/optimization101_tripleCH_checkpoints.py
@@ -0,0 +1,162 @@
+import torch 
+from torch import nn
+from torch import optim
+from torchvision import transforms
+
+from resnet101inter_linknet_model import ResNetLinkModel
+from helper import jaccard, dice, save_model, save_checkpoint, load_checkpoint
+from dataloader_inter_contour import DatasetCells, CellTrainValidLoader
+#import encoding
+from parallel import DataParallelModel, DataParallelCriterion
+
+import time
+import copy
+from tqdm import tqdm
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--lastepoch", type=int, default=0,
+    help="use this if you need to continue training a model")
+args = vars(ap.parse_args())
+
+use_cuda = torch.cuda.is_available()
+# Hyperparameters
+batch_size = 2
+nr_epochs = 250
+momentum = 0.95
+lr_rate = 0.03
+milestones = [5, 7, 8, 10, 12, 14, 16, 17, 18]
+img_size = 384
+gamma = 0.5
+
+#use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#use_cuda = False
+
+segm_model = ResNetLinkModel(input_channels=1, pretrained=True, num_classes=3)
+
+if torch.cuda.device_count() > 1:
+     #dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+     #segm_model = nn.DataParallel(segm_model)
+     #segm_model = encoding.parallel.DataParallelModel(segm_model, device_ids=[0,1,2,3,4,5,6,7])
+    segm_model = DataParallelModel(segm_model)
+print("Let's use", torch.cuda.device_count(), "GPUs!")
+segm_model.to(device)
+
+
+'''if use_cuda:
+    segm_model.cuda()
+seg_model = nn.DataParallel(seg_model)'''
+
+mul_transf = [ transforms.Resize(size=(img_size, img_size)), transforms.ToTensor() ]
+#optimizer = optim.SGD(segm_model.parameters(), lr=lr_rate, momentum=momentum)
+optimizer= optim.Adam(segm_model.parameters(), lr = 0.0001)
+#criterion = nn.BCEWithLogitsLoss().cuda() if use_cuda else nn.BCEWithLogitsLoss()
+criterion = nn.BCEWithLogitsLoss()
+criterion = DataParallelCriterion(criterion)
+criterion.to(device)
+
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=gamma)
+
+train_loader, valid_loader = CellTrainValidLoader(data_transform=transforms.Compose(mul_transf), batch_sz=batch_size, workers=2)
+
+dict_loaders = {"train":train_loader, "valid":valid_loader}
+
+
+def train_model(cust_model, dataloaders, criterion, optimizer, num_epochs, scheduler=None):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    start_time = time.time()
+    val_acc_history = []
+    best_acc = 0.0
+    best_model_wts = copy.deepcopy(cust_model)
+    best_optimizer_wts = optim.Adam(best_model_wts.parameters(), lr = 0.0001)
+    best_optimizer_wts.load_state_dict(optimizer.state_dict())
+    start_epoch = args["lastepoch"]+1
+    if (start_epoch >1):
+        filepath= "./checkpoint_epoch" + str(args["lastepoch"]) + ".pth"
+        #filepath="ResNet34watershedplus_linknet_50.pt"
+        cust_model, optimizer = load_checkpoint(cust_model,filepath)
+        #cust_model = load_model(cust_model,filepath)
+    for epoch in range(start_epoch-1,num_epochs,1):
+        print("Epoch {}/{}".format(epoch+1, num_epochs))
+        print("_"*15)
+        for phase in ["train", "valid"]:
+            if phase == "train":
+                cust_model.train()
+            if phase == "valid":
+                cust_model.eval()
+            running_loss = 0.0
+            jaccard_acc = 0.0
+            jaccard_acc_inter = 0.0
+            jaccard_acc_contour = 0.0
+            dice_loss = 0.0
+
+            for input_img, labels, inter, contours in tqdm(dataloaders[phase], total=len(dataloaders[phase])):
+                #input_img = input_img.cuda() if use_cuda else input_img
+                #labels = labels.cuda() if use_cuda else labels
+                #inter = inter.cuda() if use_cuda else inter
+                input_img = input_img.to(device)
+                labels = labels.to(device)
+                inter = inter.to(device)
+                contours = contours.to(device)
+                label_true=torch.cat([labels,inter,contours], 1)
+                #label_true=labels
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == "train"):
+                    out = cust_model(input_img)
+                    #preds = torch.sigmoid(out) 
+                    preds=out
+                    #print(preds.shape)
+                    loss = criterion(preds, label_true)
+                    loss = loss.mean()
+
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+                running_loss += loss.item() * input_img.size(0)
+                #print(labels.shape)
+                #preds=torch.FloatTensor(preds)
+                #print(preds)
+               #preds=torch.cat(preds) %more multiGPU
+                #print(preds.shape)
+                
+                jaccard_acc += jaccard(labels.to(device), torch.sigmoid(preds.to(device))) # THIS IS THE ONE THAT STILL IS ACCUMULATION IN ONLY ONE GPU
+                #jaccard_acc_inter += jaccard(inter.to('cpu'), torch.sigmoid(preds.to('cpu')))
+                #jaccard_acc_contour += jaccard(contours.to('cpu'), torch.sigmoid(preds.to('cpu')))
+
+                #dice_acc += dice(labels, preds)
+            
+            epoch_loss = running_loss / len(dataloaders[phase])
+            print("| {} Loss: {:.4f} |".format(phase, epoch_loss))
+            aver_jaccard = jaccard_acc / len(dataloaders[phase])
+            #aver_jaccard_inter = jaccard_acc_inter / len(dataloaders[phase])
+            #aver_jaccard_contour = jaccard_acc_contour / len(dataloaders[phase])
+            #aver_dice = dice_acc / len(dataloaders[phase])
+            print("| {} Loss: {:.4f} | Jaccard Average Acc: {:.4f} | ".format(phase, epoch_loss, aver_jaccard))
+            #print("| {} Loss: {:.4f} | Jaccard Average Acc: {:.4f} | Jaccard Average Acc inter: {:.4f}  | Jaccard Average Acc contour: {:.4f}| ".format(phase, epoch_loss, aver_jaccard, aver_jaccard_inter, aver_jaccard_contour))
+            print("_"*15)
+            if phase == "valid" and aver_jaccard > best_acc:
+                best_acc = aver_jaccard
+                best_acc_inter = aver_jaccard ## aver_jaccard_inter
+                best_epoch_loss = epoch_loss
+                #best_model_wts = copy.deepcopy(cust_model.state_dict)
+                best_model_wts = copy.deepcopy(cust_model)
+                best_optimizer_wts = optim.Adam(best_model_wts.parameters(), lr = 0.0001)
+                best_optimizer_wts.load_state_dict(optimizer.state_dict())
+            if phase == "valid":
+                val_acc_history.append(aver_jaccard)
+        print("^"*15)
+        save_checkpoint(best_model_wts,best_optimizer_wts,epoch+1,best_epoch_loss,best_acc,best_acc_inter)
+        print(" ")
+        scheduler.step()
+    time_elapsed = time.time() - start_time
+    print("Training Complete in {:.0f}m {:.0f}s".format(time_elapsed//60, time_elapsed % 60))
+    #print("Best Validation Accuracy: {:.4f}".format(best_acc))
+    #este no#best_model_wts = copy.deepcopy(cust_model.state_dict())
+    cust_model.load_state_dict(best_model_wts.state_dict())
+    return cust_model, val_acc_history
+
+segm_model, acc = train_model(segm_model, dict_loaders, criterion, optimizer, nr_epochs, scheduler=scheduler)
+save_model(segm_model, name="ResNet101inter_linknet_250.pt")
